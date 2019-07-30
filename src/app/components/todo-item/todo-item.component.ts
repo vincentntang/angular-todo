f0a1dc208dfbb479f95f5723b2615cc3de2c61a0
vincenttang <10290348+vincentntang@users.noreply.github.com>
2019-07-29 23:22:28 -0400

    40 min mark

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css'],
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  // Set Dynamic Classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-completed': this.todo.completed,
+    };
+
+    return classes;
+  }
+}
---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.todos = [
+      {
+        id: 1,
+        title: 'Todo One',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Todo Two',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Todo Three',
+        completed: false,
+      },
+    ];
+  }
+}
